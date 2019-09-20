1e04f9f45a2471fc71b6de409fa87a239eeaf4ca
goll_th <thore.goll@franka.de>
2019-09-20 17:07:51 +0200

    add autocargo function

--- setup.bash
+++ setup.bash
@@ -10,6 +10,7 @@ ensure-installed zsh
 ensure-installed cmake
 ensure-installed python3-dev
 ensure-installed powerline
+ensure-installed fswatch
 
 [[ ! -d ~/.oh-my-zsh ]] && sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
