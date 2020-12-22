2a9cffd0b84a7ae16967eb8ab226558938179b78
Thore Goll <thoregoll@googlemail.com>
2020-12-22 14:44:54 +0100

    FIX: Port setup.bash to OSX

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$(dirname "$(readlink -f "$0")")"
+DIR="$HOME/$(dirname "$0")"
 source $DIR/.functions
 
 # Install ZSH and OH-MY-ZSH
@@ -8,27 +8,51 @@ ensure-installed git
 ensure-installed wget
 ensure-installed zsh
 ensure-installed cmake
-ensure-installed python3-dev
-ensure-installed powerline
-ensure-installed inotify-tools
-ensure-installed autojump
+ensure-installed python3
+
+# Update PIP if required
+pip install --upgrade pip
+pip install --upgrade pip
+
+# Install ipython CLI
+pip install --user ipython
+pip3 install --user ipython
+
+if [[ $(uname -s) == "Linux" ]]; then
+    ensure-installed python3-dev
+    ensure-installed powerline
+fi
+
+if [[ $(uname -s) == "Darwin" ]]; then
+    ensure-installed ip iproute2mac # for IP utils
+    ensure-installed watch
+fi
+
+
+# Create ~/.bin folder if not yet present
+mkdir -p ~/.bin
 
 # Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+[[ ! -d ~/.nvm ]] && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 
+# Install OH MY ZSH
 [[ ! -d ~/.oh-my-zsh ]] && sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-mkdir -p ~/.bin && cd ~/.bin && wget git.io/trans && chmod +x ~/.bin/trans
+# Install translate CLI
+[[ ! -f ~/.bin/trans ]] && cd ~/.bin && wget git.io/trans && chmod +x ~/.bin/trans
 
-# Create symlinks for
+# Create symlinks
 cd ~
 ln -s -f $DIR/.zshrc      ~/.zshrc
 ln -s -f $DIR/.functions  ~/.functions
 ln -s -f $DIR/.aliases    ~/.aliases
-ln -s -f $DIR/catkin/01-clang-aliases.yaml ~/.config/catkin/verb_aliases/01-clang-aliases.yaml
+ln -s -f $DIR/ips.py      ~/.bin/ips
+ln -s -f $DIR/disk-cleanup.bash ~/.bin/disk-cleanup
 
 # Install Fuzzy finding
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [[ ! -d ~/.fzf ]]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --no-update-rc --key-bindings --completion
+fi
 
 zsh  # Restart ZSH with new config
