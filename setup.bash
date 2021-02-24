b02673db82dd9fe179c3f4da1a3123bfa886b010
Thore Goll <thoregoll@googlemail.com>
2021-02-24 12:02:43 +0100

    ADD: Compile CCLS Language Server

--- setup.bash
+++ setup.bash
@@ -59,6 +59,20 @@ fi
 [[ ! -d ~/.emacs.d ]]     && git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d --branch develop
 [[ ! -d ~/.spacemacs.d ]] && git clone https://github.com/gollth/.spacemacs.d ~/.spacemacs.d --recursive
 [[ ! -f ~/.spacemacs ]]   && ln -sf ~/.spacemacs.d/.spacemacs ~/.spacemacs
+if [ ! -d ~/.spacemacs.d/ccls/build ] ; then
+  echo "Now building CCLS C/C++ Language Server"
+  pushd `pwd`
+  cd ~/.spacemacs.d/ccls
+  if [ -d llvm ]; then rm -Rf llvm; fi
+  mkdir -p llvm && cd llvm || return
+  LLVM_BASENAME="clang+llvm-9.0.0-$(uname -m)-linux-gnu-ubuntu-$(lsb_release -rs)"
+  wget "https://releases.llvm.org/9.0.0/${LLVM_BASENAME}.tar.xz"
+  tar -xf "${LLVM_BASENAME}.tar.xz"
+  cd ..
+  cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="./llvm/${LLVM_BASENAME}"
+  cmake --build Release -- "-j$(nproc)"
+  cd `popd`
+fi
 
 # Install NVM & NPM
 [[ ! -d ~/.nvm ]] && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
